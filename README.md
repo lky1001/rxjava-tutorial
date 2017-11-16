65b8e295706096a9638649a9376d2427fd43297b
kai.1001 <kai.1001@kakaocorp.com>
2017-11-16 15:32:26 +0900

    간단한 예제 작성

--- README.md
+++ README.md
@@ -10,3 +10,6 @@
 - RxJava를 활용한 리액티브 프로그래밍
 - http://reactivex.io
 - https://github.com/ReactiveX/RxJava/wiki/What%27s-different-in-2.0
+
+## 예제
+- ObservableExample.testObservable 간단한 옵저버블 예제, 생성된 스레이드에서 Observable과 Observer가 동작.
--- src/main/java/com/tistory/lky1001/rxjavatutorial/ObservableExample.java
+++ src/main/java/com/tistory/lky1001/rxjavatutorial/ObservableExample.java
@@ -1,11 +1,64 @@
 package com.tistory.lky1001.rxjavatutorial;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.annotations.SchedulerSupport;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 /**
  * Created by kai.1001 on 2017. 11. 16..
  */
 public class ObservableExample {
 
     public static void main(String[] args) {
+        ObservableExample observableExample = new ObservableExample();
+        observableExample.testObservable();
+    }
 
+    public void testObservable() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> observer) {
+                try {
+                    System.out.println("subscribe :" + Thread.currentThread().getName());
+
+                    if (!observer.isDisposed()) {
+                        for (int i = 0; i < 5; i++) {
+                            observer.onNext(i);
+                        }
+
+                        observer.onComplete();
+                    }
+                } catch (Exception e) {
+                    if (!observer.isDisposed()) {
+                        observer.onError(e);
+                    }
+                }
+            }
+        })
+        .subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                System.out.println("onSubscribe : " + d.isDisposed());
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println(Thread.currentThread().getName() + " : " + integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        });
     }
 }
